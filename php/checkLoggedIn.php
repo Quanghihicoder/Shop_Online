4acbb5a25372b9d91e8679bcae18bd63182d72fa
Quanghihicoder <hoangquang2508@gmail.com>
2024-08-26 00:04:47 +1000

    test

--- php/checkLoggedIn.php
+++ php/checkLoggedIn.php
@@ -1,27 +1,26 @@
 <?php
 // Get the configuration file including php version and running operating system
-require("config.php");
+// require("config.php");
 
 // // start the session
 session_start(); 
 
 // Response to JavaScript in XML format
-$xmlResponse = new DOMDocument;
-$xmlResponse->formatOutput=true;
+// $xmlResponse = new DOMDocument;
+// $xmlResponse->formatOutput=true;
 
-// // Check if the user logged in
-if (isset($_SESSION["user_id"])) { 
-    $loggedIn = $xmlResponse->createElement("loggedIn");
-    $xmlResponse->appendChild($loggedIn);
-    $loggedIn->appendChild($xmlResponse->createTextNode("true"));
+// // // Check if the user logged in
+// if (isset($_SESSION["user_id"])) { 
+//     $loggedIn = $xmlResponse->createElement("loggedIn");
+//     $xmlResponse->appendChild($loggedIn);
+//     $loggedIn->appendChild($xmlResponse->createTextNode("true"));
     
-}  else {
-    $loggedIn = $xmlResponse->createElement("loggedIn");
-    $xmlResponse->appendChild($loggedIn);
-    $loggedIn->appendChild($xmlResponse->createTextNode("false"));
-}
+// }  else {
+//     $loggedIn = $xmlResponse->createElement("loggedIn");
+//     $xmlResponse->appendChild($loggedIn);
+//     $loggedIn->appendChild($xmlResponse->createTextNode("false"));
+// }
 
-echo $xmlResponse->saveXML();
-
-exit(0)
+// echo $xmlResponse->saveXML();
+echo isset($_SESSION["user_id"]);
 ?>
--- scripts/index.js
+++ scripts/index.js
@@ -17,17 +17,19 @@ const checkLoggedIn = () => {
     xhr.onreadystatechange = function() {
         if ((xhr.readyState == 4) && (xhr.status == 200)) {
             if (xhr.responseText.length > 0) {
-                parser = new DOMParser();
-                xmlDoc = parser.parseFromString(xhr.responseText, "text/xml");
+                // parser = new DOMParser();
+                // xmlDoc = parser.parseFromString(xhr.responseText, "text/xml");
     
-                if (xmlDoc.getElementsByTagName("loggedIn").length > 0) {
-                    if (xmlDoc.getElementsByTagName("loggedIn")[0].childNodes[0].nodeValue == "false") {
-                        logoutBtn.remove()
-                        logoutScript.remove()
-                    } else {
-                        loginBtn.remove()
-                    }
-                } 
+                // if (xmlDoc.getElementsByTagName("loggedIn").length > 0) {
+                //     if (xmlDoc.getElementsByTagName("loggedIn")[0].childNodes[0].nodeValue == "false") {
+                //         logoutBtn.remove()
+                //         logoutScript.remove()
+                //     } else {
+                //         loginBtn.remove()
+                //     }
+                // } 
+
+                console.log(xhr.responseText)
             }                        
         }
     };
